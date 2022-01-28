cd80968fcf7ddd0dcc7b4be648635408646e4b58
Mikhail <m.kirichenko@rj-games.com>
2022-01-28 22:31:37 +0300

    Clean
    Remove comments and not actual usings.

--- Assets/Scripts/View/UnitView.cs
+++ Assets/Scripts/View/UnitView.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Core;
 using UnityEngine;
 using UnityObject = UnityEngine.Object;
@@ -11,45 +7,6 @@ namespace View
 {
     public class UnitView
     {
-        /*private static readonly Dictionary<TeamFlag, GameObject> Originals = new Dictionary<TeamFlag, GameObject>();
-
-        private static string GetPrefabPath(TeamFlag teamFlag)
-        {
-            switch (teamFlag)
-            {
-                case TeamFlag.Blue: return "Prefabs/BlueTeam/Blue";
-                case TeamFlag.Red:  return "Prefabs/RedTeam/Red";
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(teamFlag), teamFlag, null);
-            }
-        }
-
-        public static Task Preload(CancellationToken ct)
-        {
-            var tasks = new List<Task>();
-            var flags = Enum.GetValues(typeof(TeamFlag)).Cast<TeamFlag>().ToArray();
-            foreach (var flag in flags)
-            {
-                tasks.Add(Task.Run(() =>
-                {
-                    var loadPref = Resources.LoadAsync<GameObject>(GetPrefabPath(flag));
-                    while (!loadPref.isDone)
-                    {
-                        return null;
-                    }
-
-                    var go = (GameObject)loadPref.asset;
-                    Originals.Add(flag, go);
-                    
-                    return go;
-                }, ct));
-            }
-
-            Task.WaitAll(tasks.ToArray());
-            
-            return null;
-        }*/
-        
         public static UnitView Create(TeamFlag teamFlag, Transform parent)
         {
             GameObject unit = null;
